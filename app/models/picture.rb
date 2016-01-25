1e582525b420f4b3773ec44e8cd2b023e248b11d
David Dongseung Lee <dleeprograms@outlook.com>
2016-01-25 16:13:46 -0500

    Making pictures show at /pictures

--- app/models/picture.rb
+++ app/models/picture.rb
@@ -1,18 +1,4 @@
-class CreatePicture < ActiveRecord::Migration
-  def change
-    create_table :pictures do |t|
-      t.string :artist
-      t.string :title
-      t.string :url
-
-      t.timestamps null: false
-    end
-  end
-
-
-
-
-
+class Picture < ActiveRecord::Base
 
 
 end
